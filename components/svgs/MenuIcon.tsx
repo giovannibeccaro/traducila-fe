737f0e534d6872eb975bc199988ed5c66f149e21
Giovanni Beccaro <gio.beccaro@gmail.com>
2022-11-24 15:02:56 +0100

    home and navbar, static only

---
+++ components/svgs/MenuIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const MenuIcon = () => {
+  return (
+    <>
+      <svg
+        width="32"
+        height="21"
+        viewBox="0 0 32 21"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M15.9988 20.5H0.812988V17.0833H15.9988V20.5ZM31.1846 11.9583H0.812988V8.54167H31.1846V11.9583ZM31.1846 3.41667H15.9988V0H31.1846V3.41667Z"
+          fill="#2C2C2C"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default MenuIcon;
---
+++ components/svgs/SearchIcon.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+type Props = {
+  color: string;
+};
+
+const SearchIcon: React.FC<Props> = ({ color }) => {
+  return (
+    <>
+      <svg
+        width="25"
+        height="25"
+        viewBox="0 0 25 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.8467 17.1851L22.0003 22.5442"
+          stroke={color}
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10.4064 18.9706C14.913 18.9706 18.5663 15.1716 18.5663 10.4853C18.5663 5.799 14.913 2 10.4064 2C5.89987 2 2.24658 5.799 2.24658 10.4853C2.24658 15.1716 5.89987 18.9706 10.4064 18.9706Z"
+          stroke={color}
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default SearchIcon;
