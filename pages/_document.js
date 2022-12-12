d4063d31ee075bf8115786e9d4bae552176195b0
Giovanni Beccaro <gio.beccaro@gmail.com>
2022-12-12 18:24:58 +0100

    changed roboto weight

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Inter:wght@300;400;500;600;700;800&family=Poppins&family=Roboto+Condensed:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Fredoka+One&family=Inter:wght@300;400;500;600;700;800&family=Poppins&family=Roboto+Condensed:wght@400;600&display=swap"
           rel="stylesheet"
         />
       </Head>
